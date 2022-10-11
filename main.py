4ce69b906f86d8639105a0c8e132e5fc9b50cf10
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-11 02:23:48 +0000

    7-6

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
+if len(num1) < len(num2):
+    num1.extend(num2)
+    print(num1)
+else:
+    num2.extend(num1)
+    print(num2)
 
 # ******************************
 # Make your Code
